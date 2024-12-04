9cb8feab6d60abb95df8b670668c3aa8608947a1
Leo Carrasco <lcarrasco.dev@gmail.com>
2024-12-03 22:55:55 -0300

    Incorporate Java 17 project based on Spring Initializr

---
+++ src/test/java/com/arael82/challenge/artists/ArtistsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arael82.challenge.artists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtistsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
